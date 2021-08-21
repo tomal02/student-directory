3767c5cf1ece1a209e3d0072c98b2ae1e5a017b0
Tom Lee <tomadamlee02@gmail.com>
2021-08-21 15:24:00 +0100

    added comments to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# then we print the student count
 print "Overall we have "
+# it's important print() doesn't add new line characters (\n)
 print "11"
 puts " great students"
