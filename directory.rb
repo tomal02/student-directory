b152cf1936216864a39d00fc334b80695bc9d25f
Tom Lee <tomadamlee02@gmail.com>
2021-08-21 17:40:51 +0100

    moved main calls into interactive_menu method and added a couple of \n's to improve presentation

--- directory.rb
+++ directory.rb
@@ -18,30 +18,57 @@ def input_students
     else
       students << {name: name, cohort: current_month}
     end
-    puts "Now we have #{students.count} students."
+    puts "Now we have #{students.count} student(s)."
     # gets another name from the user
     name = gets.chomp
   end
   # returns students array
   students
 end
-# method to print the header
+
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we want room to add more items
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # terminates program
+    else
+      puts "I don't know what you mean, please try again." # displays error
+    end
+  end
+end
+
+  # method to print the header
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 # method that goes through every element and prints the hashes correctly
 def print(students)
-  students.each_with_index do |student, i|
-    puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  if students.empty?
+    puts "No students currently enrolled :("
+    exit
+  else
+    students.each_with_index do |student, i|
+      puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
+
+  students.map
 end
 # method that prints the footer, which contains the student count
 def print_footer(students)
   puts "Overall we have #{students.count} great students"
 end
-# calls the methods in order
-students = input_students
-print_header
-print(students)
-print_footer(students)
+# calls the interactive_menu method
+interactive_menu
