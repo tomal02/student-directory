8dcdd7367c1de35a7d4dfb9f7173f629b0d9fcca
Tom Lee <tomadamlee02@gmail.com>
2021-08-22 00:27:23 +0100

    added save to file function

--- directory.rb
+++ directory.rb
@@ -24,6 +24,17 @@ def input_students
   end
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def interactive_menu
   loop do # infinite loop until user exits program
     print_menu
@@ -35,6 +46,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list of students to students.csv"
   puts "9. Exit" # 9 because we want room to add more items
 end
 
@@ -56,6 +68,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # terminates program
   else
