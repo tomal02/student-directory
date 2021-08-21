015a1fdd5239a00fec59f4d932b5147f67ba6592
Tom Lee <tomadamlee02@gmail.com>
2021-08-21 16:17:44 +0100

    changed the hash to be user defined

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
-# first we define the array of hashes for the students
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
+# first we get input to define the array of hashes for the students
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #initialises student array
+  students = []
+  # gets the name from the user
+  name = gets.chomp
+  # whilst name is not empty, repeat this code
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students."
+    # gets another name from the user
+    name = gets.chomp
+  end
+  # returns students array
+  students
+end
 # method to print the header
 def print_header
   puts "The students of Villains Academy"
@@ -28,6 +32,7 @@ def print_footer(students)
   puts "Overall we have #{students.count} great students"
 end
 # calls the methods in order
+students = input_students
 print_header
 print(students)
 print_footer(students)
